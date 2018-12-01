f8510a693d141406f27f0c5057ea5f269d0c475c
trentbaldwin <trentbaldwin@10.124.181.40>
2018-12-01 16:56:20 -0600

    Messages

--- LeetHacksSimulator/src/GameLogic/MsgData.java
+++ LeetHacksSimulator/src/GameLogic/MsgData.java
@@ -8,5 +8,9 @@ public class MsgData {
 		msgList = new LinkedList<String>();
 		msgList.add(0, "WARNING: THE CIA IS WATCHING");
 		msgList.add(1, "PLEASE KILL YOURSELF SLOWLY ;)");
+		msgList.add(2, "HERE COMES THE FUCKIN REDS");
+		msgList.add(3, "AHHHHHHHHHHHHH");
+		msgList.add(4, "Dair motherfucker");
+		msgList.add(5, "Pineapple");
 	}
 }
