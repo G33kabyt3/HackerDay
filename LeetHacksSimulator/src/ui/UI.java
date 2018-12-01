8d97f2f14aa328fac872bf77602c854f565697a1
G33kabyt3 <trapperross@gmail.com>
2018-12-01 16:03:41 -0600

    DAIR

--- LeetHacksSimulator/src/ui/UI.java
+++ LeetHacksSimulator/src/ui/UI.java
@@ -5,35 +5,53 @@ import java.awt.Dimension;
 import java.awt.event.AWTEventListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.Reader;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.xml.bind.Marshaller.Listener;
 
+import GameLogic.GameLogic;
+import GameLogic.Player;
+
 public class UI {
   
-public static void main(String[]args)
-{
- 
-}
+  
 
- /* static JFrame frame;
+ 
+
+  static JFrame frame;
   public static void main(String[] args) {
+    Player p  = new Player("P1");
+    GameLogic g = new GameLogic();
     frame = new JFrame("Hacker");
     frame.setVisible(true);
     frame.setSize(new Dimension(600,600));
     frame.setFocusable(true);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.getContentPane().setBackground(Color.BLACK);
-    addLabel("Hello");
-    addLabel("Testing");
+    
+    updateLabel("Testing");
+    try {
+      Thread.sleep(10000);
+    } catch (Exception e)
+    {
+      
+    }
+    updateLabel("H");
+    
     frame.addKeyListener(new KeyListener(){
          @Override
             public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode() == KeyEvent.VK_UP){
-                    System.out.println("Hi");
+                
+                   p.insert(String.valueOf(e.getKeyChar()));
+                   g.CalcScore(p);
+                   System.out.println(p.score);
                    
-                }
+                 
             }
 
             @Override
@@ -50,14 +68,16 @@ public static void main(String[]args)
 
 }
   
-  public static void addLabel(String s)
+  public static void updateLabel(String s)
   {
     JLabel label = new JLabel(s);
     label.setForeground(Color.GREEN);
-    frame.getContentPane().add(new JLabel(s));
+    frame.add(label);
     label.setText(s); 
   }
-  */
+  
+
+
 
 
 }
