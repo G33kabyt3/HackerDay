c3e10beab3f671ae6a116647f6c780fcb04df304
G33kabyt3 <trapperross@gmail.com>
2018-12-01 13:57:05 -0600

    Stuff

---
+++ LeetHacksSimulator/src/ui/UI.java
@@ -0,0 +1,63 @@
+package ui;
+
+import java.awt.*;
+import java.awt.Dimension;
+import java.awt.event.AWTEventListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.xml.bind.Marshaller.Listener;
+
+public class UI {
+  
+public static void main(String[]args)
+{
+ 
+}
+
+ /* static JFrame frame;
+  public static void main(String[] args) {
+    frame = new JFrame("Hacker");
+    frame.setVisible(true);
+    frame.setSize(new Dimension(600,600));
+    frame.setFocusable(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().setBackground(Color.BLACK);
+    addLabel("Hello");
+    addLabel("Testing");
+    frame.addKeyListener(new KeyListener(){
+         @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_UP){
+                    System.out.println("Hi");
+                   
+                }
+            }
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+               
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+              
+            }
+    });
+    frame.setVisible(true);
+
+}
+  
+  public static void addLabel(String s)
+  {
+    JLabel label = new JLabel(s);
+    label.setForeground(Color.GREEN);
+    frame.getContentPane().add(new JLabel(s));
+    label.setText(s); 
+  }
+  */
+
+
+}
