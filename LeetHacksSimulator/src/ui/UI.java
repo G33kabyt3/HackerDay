75f59483525a0f34288452d3bf5bb4c25f04eff7
G33kabyt3 <trapperross@gmail.com>
2018-12-01 17:49:31 -0600

    WAKE ME UP

--- LeetHacksSimulator/src/ui/UI.java
+++ LeetHacksSimulator/src/ui/UI.java
@@ -9,6 +9,11 @@ import java.io.BufferedReader;
 import java.io.Console;
 import java.io.Reader;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,37 +25,36 @@ import GameLogic.Player;
 public class UI {
   
   
-
- 
+  static Player p  = new Player("P1");
+  static GameLogic g = new GameLogic();
 
   static JFrame frame;
   public static void main(String[] args) {
-    Player p  = new Player("P1");
-    GameLogic g = new GameLogic();
+    Timer timer = new Timer();
     frame = new JFrame("Hacker");
     frame.setVisible(true);
-    frame.setSize(new Dimension(600,600));
+    frame.setSize(new Dimension(900,900));
     frame.setFocusable(true);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.getContentPane().setBackground(Color.BLACK);
     
-    updateLabel("Testing");
-    try {
-      Thread.sleep(10000);
-    } catch (Exception e)
-    {
-      
+    for(int i =0; i<10000; i+=3000)
+    { 
+      if (Math.random()<0.9)
+        timer.schedule(new RandomMessage(frame, p,g), i);
     }
-    updateLabel("H");
+    timer.schedule(new GameOver(frame, p, g), 10000);
+
     
     frame.addKeyListener(new KeyListener(){
          @Override
             public void keyPressed(KeyEvent e) {
                 
-                   p.insert(String.valueOf(e.getKeyChar()));
-                   g.CalcScore(p);
-                   System.out.println(p.score);
-                   
+                  /* p.insert(String.valueOf(e.getKeyChar()));
+                   *g.CalcScore(p);
+                   *System.out.println(p.score);
+                   **/
+                   System.out.println("a");
                  
             }
 
@@ -64,8 +68,7 @@ public class UI {
               
             }
     });
-    frame.setVisible(true);
-
+timer.purge();
 }
   
   public static void updateLabel(String s)
@@ -77,8 +80,44 @@ public class UI {
     label.setText(s); 
   }
   
+  public static void eventTrigger()
+  {
+    
+  }
+  
+  
+}
 
 
+class GameOver extends TimerTask {
+  JFrame frame; 
+  Player p;
+  GameLogic g;
+  public GameOver(JFrame frame, Player p, GameLogic g)
+  {
+    this.frame = frame;
+    this.p = p;
+    this.g = g;
+  }
+  public void run() {
+     frame.dispose();
+     System.out.println("Congrats, your score is: "+ p.score);
+    
+  }
+}
 
-
+class RandomMessage extends TimerTask {
+  
+  JFrame frame; 
+  Player p;
+  GameLogic g;
+  public RandomMessage(JFrame frame, Player p, GameLogic g)
+  {
+    this.frame = frame;
+    this.p = p;
+    this.g = g;
+  }
+  public void run() {
+     g.RandomMessage();
+  }
 }
