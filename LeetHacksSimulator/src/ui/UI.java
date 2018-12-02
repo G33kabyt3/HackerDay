f108b0afa144d4e1b30606e0c764f12d96b116b0
G33kabyt3 <trapperross@gmail.com>
2018-12-01 18:09:15 -0600

    The nuke.

--- LeetHacksSimulator/src/ui/UI.java
+++ LeetHacksSimulator/src/ui/UI.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.event.AWTEventListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.Console;
 import java.io.Reader;
@@ -27,6 +28,7 @@ public class UI {
   
   static Player p  = new Player("P1");
   static GameLogic g = new GameLogic();
+  static int roundTime= 10000;
 
   static JFrame frame;
   public static void main(String[] args) {
@@ -37,38 +39,35 @@ public class UI {
     frame.setFocusable(true);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.getContentPane().setBackground(Color.BLACK);
+    frame.addKeyListener(new KeyListener(){
+      @Override
+         public void keyPressed(KeyEvent e) {
+             
+                p.insert(String.valueOf(e.getKeyChar()));
+                g.CalcScore(p);
+                System.out.println(p.score);
+             
+              
+         }
+
+         @Override
+         public void keyTyped(KeyEvent e) {
+            
+         }
+
+         @Override
+         public void keyReleased(KeyEvent e) {
+           
+         }
+ });
     
-    for(int i =0; i<10000; i+=3000)
+    for(int i =0; i<roundTime; i+=1000)
     { 
       if (Math.random()<0.9)
         timer.schedule(new RandomMessage(frame, p,g), i);
     }
-    timer.schedule(new GameOver(frame, p, g), 10000);
+    timer.schedule(new GameOver(frame, p, g), roundTime);
 
-    
-    frame.addKeyListener(new KeyListener(){
-         @Override
-            public void keyPressed(KeyEvent e) {
-                
-                  /* p.insert(String.valueOf(e.getKeyChar()));
-                   *g.CalcScore(p);
-                   *System.out.println(p.score);
-                   **/
-                   System.out.println("a");
-                 
-            }
-
-            @Override
-            public void keyTyped(KeyEvent e) {
-               
-            }
-
-            @Override
-            public void keyReleased(KeyEvent e) {
-              
-            }
-    });
-timer.purge();
 }
   
   public static void updateLabel(String s)
@@ -78,11 +77,6 @@ timer.purge();
     label.setForeground(Color.GREEN);
     frame.add(label);
     label.setText(s); 
-  }
-  
-  public static void eventTrigger()
-  {
-    
   }
   
   
@@ -100,8 +94,12 @@ class GameOver extends TimerTask {
     this.g = g;
   }
   public void run() {
-     frame.dispose();
-     System.out.println("Congrats, your score is: "+ p.score);
+    System.out.println("Congrats, your score is: "+ p.score);
+    frame.getContentPane().removeAll();
+    frame.dispose();
+    frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+     
+     
     
   }
 }
